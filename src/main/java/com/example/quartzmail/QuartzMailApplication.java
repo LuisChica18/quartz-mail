b251eba33f23094b1ebd31ea7cd2178f8d71ed09
Luis Chica Moncayo <luischicamoncayo@hotmail.com>
2022-12-20 20:04:55 -0500

    Initial commit

---
+++ src/main/java/com/example/quartzmail/QuartzMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.quartzmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quartzmail/QuartzMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
